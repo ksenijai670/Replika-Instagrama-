34dcf1a8900294029e0ead0af552fd4e88244d16
AlexaMilenkovic <150688558+AlexaMilenkovic@users.noreply.github.com>
2026-03-14 14:09:09 +0100

    Adding cors module

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const authMiddleware = require('./middleware/authMiddleware');
@@ -10,6 +11,7 @@ const errorHandler = require('./middleware/errorHandler');
 const services = require('./config/services');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 4000;
 
 app.use(rateLimiter);
